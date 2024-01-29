f7449dde9816b3016ae5d7450b3df74d032b133f
jcao-ai <tsaojiaxin@gmail.com>
2024-01-29 06:50:18 +0000

    add: sm89 for fp8_e4m3_cvt

--- lib/Conversion/TritonGPUToLLVM/ElementwiseOpToLLVM.cpp
+++ lib/Conversion/TritonGPUToLLVM/ElementwiseOpToLLVM.cpp
@@ -879,7 +879,7 @@ struct FpToFpOpConversion
       llvm::errs() << "\n";
       llvm::report_fatal_error("Unsupported rounding mode for conversion.");
     }
-    if (computeCapability < 90 &&
+    if (computeCapability < 89 &&
         (srcTy.isFloat8E4M3FNUZ() || dstTy.isFloat8E4M3FNUZ())) {
       llvm::errs() << "Conversion from/to f8e4m3nv is only supported on "
                       "compute capability >= 90"
